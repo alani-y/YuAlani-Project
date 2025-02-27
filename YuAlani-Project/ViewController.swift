829dc05277d2eecc1a2320b4e71cca83979dd761
Alani Yu <alani.yu79@utexas.edu>
2025-02-26 22:53:17 -0600

    added rooms

--- YuAlani-Project/ViewController.swift
+++ YuAlani-Project/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var floorPlan = [Room]();
+    
+    var room1: Room?
+    var room2: Room?
+    var room3: Room?
+    var room4: Room?
+    var room5: Room?
+    var room6: Room?
+    var room7: Room?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
